8e4847caf8115a34d993d955d947187f1a905876
victor 406 <victor.ezaki@alunos.sc.senac.br>
2025-11-12 20:58:01 -0300

    ajuste no header

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@ Route::view('/receita', 'receita')->middleware('auth');
 Route::get('/registrar', [RegistrarUsuarioController::class, 'create']);
 Route::post('/registrar', [RegistrarUsuarioController::class, 'store']);
 
-Route::get('/login', [SessaoUsuarioController::class, 'create'])->name('login');
+Route::get('/', [SessaoUsuarioController::class, 'create'])->name('login');
 Route::post('/login', [SessaoUsuarioController::class, 'store']);
 Route::post('/logout', [SessaoUsuarioController::class, 'destroy']);
